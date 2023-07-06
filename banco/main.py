7c3b8e39b1f82981f69b3b832a2f2030db371b24
ThiagoPrIn <138177232+ThiagoPrIn@users.noreply.github.com>
2023-07-06 01:04:21 -0300

    Add lib 'os', refactor: menu de saque/deposito.

--- banco/main.py
+++ banco/main.py
@@ -1,5 +1,6 @@
 from random import choice
 from complementos import menu, erro_cor
+import os
 
 
 class Banco:
@@ -18,28 +19,34 @@ class Banco:
         if valor_deposito > 0:
             self.__saldo += valor_deposito
             print(f'\nDEPÓSITO EFETUADO COM SUCESSO! R$+{valor_deposito:.2f}\n'
-                  f'SALDO: R${self.__saldo:.2f}')
+                  f'SALDO: R${self.__saldo:.2f}\n')
+            os.system('pause')
         else:
-            print(erro_cor('ERRO! Valores negativos não é válido para depósito'))
+            print(erro_cor('ERRO! Valores negativos não é válido para depósito\n'))
+            os.system('pause')
 
     def sacar(self, valor_saque):
         if valor_saque <= self.__saldo:
             self.__saldo -= valor_saque
             print(f'\nSAQUE EFETUADO COM SUCESSO! R$-{valor_saque:.2f}\n'
-                  f'SALDO: R${self.__saldo:.2f}')
+                  f'SALDO: R${self.__saldo:.2f}\n')
+            os.system('pause')
         else:
-            print(erro_cor('ERRO! Saldo insuficiente'))
+            print(erro_cor('ERRO! Saldo insuficiente\n'))
+            os.system('pause')
 
 
 contas_clientes = {}
 while True:
-    options = menu('Criar Conta', 'Acessar Conta', 'Encerrar Sessão')
+    os.system('cls')
+    options = menu('ABRIR CONTA', 'ACESSAR CONTA', 'ENCERRAR SESSÃO')
 
     if options == 3:
         print('\nObrigado por escolher o Dalla$ Bank')
         break
 
     elif options == 1:
+        os.system('cls')
         nome_cliente = input('Nome do Cliente: ').strip().title()
         cpf_cliente = input('CPF do Cliente (ex: 000.000.000-00): ')
         deposito_inicial = float(input('Valor de Depósito: R$'))
@@ -48,27 +55,32 @@ while True:
         contas_clientes[num_conta] = Banco(nome_cliente, cpf_cliente, num_conta, deposito_inicial)
         print(f'\n|> CONTA CRIADA COM SUCESSO! <|\n'
               f'CLIENTE: {nome_cliente}    CPF: {cpf_cliente}   CONTA: {num_conta}\n'
-              f'DEPÓSITO: R${deposito_inicial:.2f}')
+              f'DEPÓSITO: R${deposito_inicial:.2f}\n')
+        os.system('pause')
 
     elif options == 2:
         buscar = input('Número da Conta: ')
+        os.system('cls')
 
-        try:
-            contas_clientes[buscar].info_cliente()
-        except KeyError:
-            print(erro_cor('ERRO! Desculpe, conta não encontrada'))
-
-        else:
-            while True:
-                saque_deposito = menu('Sacar', 'Depositar', 'Voltar')
+        while True:
+            try:
+                contas_clientes[buscar].info_cliente()
+            except KeyError:
+                print(erro_cor('ERRO! Desculpe, conta não encontrada\n'))
+                os.system('pause')
+                break
+            else:
+                saque_deposito = menu('SACAR', 'DEPOSITAR', 'VOLTAR')
 
                 if saque_deposito == 3:
                     break
                 elif saque_deposito == 1:
+                    os.system('cls')
                     contas_clientes[buscar].sacar(
                         float(input('Valor a Sacar: R$'))
                     )
                 elif saque_deposito == 2:
+                    os.system('cls')
                     contas_clientes[buscar].depositar(
                         float(input('Valor a Depositar: R$'))
                     )
